fda44e76eb3294b0f80e3f733a4a194b2b2e87bb
Esteban Beltran <estebanenriquebeltran@gmail.com>
2022-08-20 21:02:15 -0400

    Add test click like button  Post.test.tsx

--- src/pages/home/test/Post.test.tsx
+++ src/pages/home/test/Post.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React from 'react'
 
 import { generatePost } from '../../../factories/post'
@@ -6,9 +7,25 @@ import Post from '../components/post'
 
 test('should show post attributes', () => {
   const post = generatePost()
-  render(<Post post={post} isFavorite={false} />)
+  render(<Post post={post} isFavorite={false} handleFavorite={() => {}} />)
 
   expect(screen.getByText(post.story_title)).toBeInTheDocument()
   expect(screen.getByText(`2 hours age by ${post.author}`)).toBeInTheDocument()
   expect(screen.getByAltText('heart icon')).toBeInTheDocument()
+})
+
+test('Clicking on the like button', async () => {
+  const user = userEvent.setup()
+  const post = generatePost()
+  const handleFavoriteMock = jest.fn()
+  render(
+    <Post post={post} isFavorite={false} handleFavorite={handleFavoriteMock} />
+  )
+
+  await user.click(
+    screen.getByRole('button', {
+      name: 'favorite',
+    })
+  )
+  expect(handleFavoriteMock).toHaveBeenCalledTimes(1)
 })
