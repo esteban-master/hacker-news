275c0f544591cdf7377af845373b14bc76ace7ea
Esteban Beltran <estebanenriquebeltran@gmail.com>
2022-08-21 01:17:24 -0400

    add second test useSelectFilter

--- src/hooks/useSelectFilter/index.tsx
+++ src/hooks/useSelectFilter/index.tsx
@@ -1,3 +1,19 @@
-export const useSelectFilter = () => {
-  return []
+import { useState } from 'react'
+
+export const useSelectFilter = (defaultValue: string) => {
+  const [value, setValue] = useState(
+    localStorage.getItem('filter')
+      ? localStorage.getItem('filter')
+      : defaultValue
+  )
+
+  const setFilter = (value: string) => {
+    localStorage.setItem('filter', value)
+    setValue(value)
+  }
+
+  return {
+    setFilter,
+    value,
+  }
 }
--- src/hooks/useSelectFilter/useSelectFilter.test.tsx
+++ src/hooks/useSelectFilter/useSelectFilter.test.tsx
@@ -1,8 +1,18 @@
 import { renderHook } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 
 import { useSelectFilter } from '.'
 
 test('mount useFavoritePosts', () => {
-  const { result } = renderHook(() => useSelectFilter())
-  expect(result.current).toEqual([])
+  const { result } = renderHook(() => useSelectFilter('reactjs'))
+  expect(result.current.value).toBe('reactjs')
+})
+
+test('select filter', () => {
+  const { result } = renderHook(() => useSelectFilter('reactjs'))
+  act(() => {
+    result.current.setFilter('vuejs')
+  })
+
+  expect(result.current.value).toBe('vuejs')
 })
