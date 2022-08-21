cd3f00dab101e5156705c90acb559d058e8067f2
Esteban Beltran <estebanenriquebeltran@gmail.com>
2022-08-20 22:59:25 -0400

    Add test Pagination and refactor pagination component

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 type Props = {
   currentPage: number
@@ -6,10 +6,21 @@ type Props = {
   onChange: (value: number) => void
 }
 
-const Pagination = ({ currentPage, totalPages, onChange }: Props) => {
-  const pageItems = [-3, -2, -1, 0, 1, 2, 3]
+const getPages = (currentPage: number, totalPages: number): number[] => {
+  return [-3, -2, -1, 0, 1, 2, 3]
     .map((v) => currentPage + v)
     .filter((page) => page > 0 && page <= totalPages)
+}
+
+const Pagination = ({ currentPage, totalPages, onChange }: Props) => {
+  const [pageItems, setPageItems] = useState<number[]>(
+    getPages(currentPage, totalPages)
+  )
+
+  useEffect(
+    () => setPageItems(getPages(currentPage, totalPages)),
+    [currentPage, totalPages]
+  )
 
   return (
     <div>
--- src/components/test/Pagination.test.tsx
+++ src/components/test/Pagination.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React from 'react'
 
 import Pagination from '../pagination'
@@ -8,4 +9,15 @@ test('should show buttons with number page', async () => {
   render(<Pagination currentPage={2} totalPages={10} onChange={() => {}} />)
 
   pages.forEach((page) => expect(screen.getByText(page)).toBeInTheDocument())
+})
+
+test('select page number 3', async () => {
+  const user = userEvent.setup()
+  const onChangeMock = jest.fn()
+  const page = 3
+  render(<Pagination currentPage={2} totalPages={10} onChange={onChangeMock} />)
+
+  await user.click(screen.getByText(page))
+  expect(onChangeMock).toHaveBeenCalledTimes(1)
+  expect(onChangeMock).toHaveBeenCalledWith(page - 1)
 })
